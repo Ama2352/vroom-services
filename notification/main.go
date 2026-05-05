456d3e8529b5bbcf3fc65481a5aa5d96756b541c
Hồ Nguyễn Minh Sang <165858646+Ama2352@users.noreply.github.com>
2026-05-05 20:34:18 +0700

    feat: add frontend to gitlab-ci

--- notification/main.go
+++ notification/main.go
@@ -68,7 +68,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		if c.Request.Method == "OPTIONS" {
